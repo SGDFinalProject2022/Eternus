9ea1e5686e1a8a29e41e6e8ff3fd085ce5567155
epicjodog <epicjodog@gmail.com>
2022-05-18 19:04:52 -0400

    Jump crouch & sprint

--- Eternus/Assets/Scripts/PlayerInteractions/MouseLook.cs
+++ Eternus/Assets/Scripts/PlayerInteractions/MouseLook.cs
@@ -9,6 +9,7 @@ public class MouseLook : MonoBehaviour
 {
     [SerializeField] float mouseSensitivity = 500f;
     [SerializeField] Transform playerBody;
+    [SerializeField] LayerMask interactableLayerMask;
 
     float xRotation = 0f;
 
@@ -21,7 +22,6 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //something makes the mouse jump..but what?
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -30,5 +30,12 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, interactableLayerMask))
+        {
+            Debug.Log(hit.collider.name);
+
+        }
     }
 }
--- Eternus/Assets/Scripts/PlayerInteractions/PlayerMovement.cs
+++ Eternus/Assets/Scripts/PlayerInteractions/PlayerMovement.cs
@@ -1,38 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Main player movement
+/// </summary>
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Character Controller")]
     [SerializeField] CharacterController controller;
-    [SerializeField] float walkingSpeed = 12f;
+
+    [Header("Player Parameters")]
+    [SerializeField] float walkingSpeed = 8f;
     [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpHeight = 1f;
+    [Header("Crouching")]
+    [SerializeField] float crouchHeight = 1f; //base is 3.8
+    [SerializeField] float crouchSpeed = 3f;
+    [Header("Sprint")]
+    [SerializeField] float sprintSpeed = 12f;
+
+    [Header("Ground Check")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
 
     Vector3 velocity;
+    bool isOnGround;
+    bool isCrouching;
+    float originalHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalHeight = controller.height;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (controller.isGrounded && velocity.y < 0)
+        //checks if it's on the ground
+        isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isOnGround && velocity.y < 0)
         {
             velocity.y = -2f;
+        }       
+
+        //jump
+        if(Input.GetAxis("Jump") > 0 && isOnGround && !isCrouching)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);           
         }
 
+        //crouch
+        float y = Input.GetAxis("Crouch");
+        controller.height = Mathf.Lerp(originalHeight, crouchHeight, y);
+        groundCheck.localPosition = new Vector3(0, Mathf.Lerp(-1.8f, -crouchHeight/2, y), 0);
+        if (y > 0)
+        {
+            isCrouching = true;
+            groundCheck.localPosition += new Vector3(groundCheck.localPosition.x, groundCheck.localPosition.y + (crouchHeight / 2), groundCheck.localPosition.z);
+        }
+        if (y == 0)
+        {
+            isCrouching = false;
+        }
+
+        //movement + sprint
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
+        float sprint = Input.GetAxis("Sprint");
+        float finalSpeed = walkingSpeed;
+        if (y > 0)
+        {
+            finalSpeed = Mathf.Lerp(walkingSpeed, crouchSpeed, y);
+        }
+        if (!isCrouching && sprint > 0)
+        {
+            finalSpeed = Mathf.Lerp(walkingSpeed, sprintSpeed, sprint);
+        }
         Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * finalSpeed * Time.deltaTime);
 
-        controller.Move(move * walkingSpeed * Time.deltaTime);
-
+        //gravity
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
 
     }
