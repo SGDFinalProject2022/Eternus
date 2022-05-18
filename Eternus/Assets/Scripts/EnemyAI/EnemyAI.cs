a7f6667a2d6bdf3224bcf0802e7aefd0820df581
vendorko <73616316+vendorko@users.noreply.github.com>
2022-05-18 13:27:04 -0400

    Fixed enemy hit function, adjusted aggro parameters, added random/reverse pathfinding

--- Eternus/Assets/Scripts/EnemyAI/EnemyAI.cs
+++ Eternus/Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -11,15 +11,25 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private float aggroRange = 3f;
     [SerializeField] private float attackRange = 3f;
     [SerializeField] private float attackTime = 3f;
+    [SerializeField] private bool randomPath;
+    [SerializeField] private bool reversePath;
     private List<Transform> nodes = new List<Transform>();
 
+    //Patrol
     NavMeshAgent ai;
     int currentNode = 0;
+    bool inReverse = false;
+
+    //Aggro
     bool isAggrod = false;
     bool playerInSight = false;
-    bool idle = false;
     bool playerIsHidden = false;
+    bool isSoundAggrod = false;
+    bool idle = false;
+  
+    //Attack
     bool isAttacking = false;
+    
     Transform player;
 
     void Awake()
@@ -31,18 +41,23 @@ public class EnemyAI : MonoBehaviour
         MoveToNextNode();
     }
 
-
     void Update()
     {
-        if(!isAggrod && Vector3.Distance(transform.position, nodes[currentNode].position) < 3f)
+        if(!isAggrod)
         {
             MoveToNextNode();
         }
+
+        if(playerInSight)
+        {
+            ai.destination = player.position;
+        }
+
         SightAggro();
         BeginDeaggro();
         AttackPlayer();
+        ArriveAtSoundAggro();
     }
-
     void SetUpNodes()
     {
         currentNode = 0;
@@ -54,22 +69,81 @@ public class EnemyAI : MonoBehaviour
 
     void MoveToNextNode()
     {
-        currentNode++;
-        if(currentNode >= nodes.Count)
+        //Check for random path parameter
+        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 3f && randomPath)
         {
-            currentNode = 0;
-        }
+            currentNode = Random.Range(0, nodes.Count);
+        }        
+        else if (Vector3.Distance(transform.position, nodes[currentNode].position) < 3f)
+        {
+            //Check for reverse path parameter
+            if(reversePath)
+            {
+                if (inReverse)
+                {
+                    currentNode--;
+                    if (currentNode < 0)
+                    {
+                        inReverse = false;
+                        currentNode = 1;
+                    }
+                }
+                else
+                {
+                    currentNode++;
+                    if (currentNode >= nodes.Count)
+                    {
+                        inReverse = true;
+                        currentNode = nodes.Count - 2;
+                    }
+                }
+            }
+            else
+            {
+                currentNode++;
+                if (currentNode >= nodes.Count)
+                {
+                    currentNode = 0;
+                }
+            }    
+        }        
         ai.destination = nodes[currentNode].position;
     }
-
-    void SoundAggro(Transform node)
+    
+    //Enemy is aggrod, moves to location of sound queue.
+    public void SoundAggro(Transform sound)
     {
         isAggrod = true;
-        ai.destination = node.position;
+        isSoundAggrod = true;
 
+        float distance = Vector3.Distance(nodes[0].position, sound.position);
+        Transform closestNode = nodes[0];
+        foreach(Transform node in nodes)
+        {
+            if(Vector3.Distance(node.position, sound.position) < distance)
+            {
+                closestNode = node;
+                distance = Vector3.Distance(node.position, sound.position);
+            }
+        }
+        ai.destination = closestNode.position;
         BeginDeaggro();
     }
 
+    //Enemy arrives at sound aggro location
+    void ArriveAtSoundAggro()
+    {
+        if(isSoundAggrod)
+        {
+            isSoundAggrod = false;
+            if(Vector3.Distance(transform.position, ai.destination) < 3f )
+            {
+                BeginDeaggro();
+            }
+        }
+    }
+
+    //If player is in sight and is within attack range, hit the player
     void AttackPlayer()
     {
         if(player != null)
@@ -85,18 +159,25 @@ public class EnemyAI : MonoBehaviour
             }
         }        
     }
-
     IEnumerator Hit()
     {
-        while(true)
+        while (true)
         {
-            yield return new WaitForSeconds(attackTime);
+            if (Vector3.Distance(transform.position, player.position) > attackRange)
+            {
+                break;
+            }
             print("Hit the player");
-        }
+            ai.speed = 0;
+            yield return new WaitForSeconds(attackTime);
+            ai.speed = movementSpeed;
+        }        
     }
 
+    //If player is within sight range and is not hidden, aggro on the player
     void SightAggro()
     {
+        //Get all objects within range
         bool inRange = false;
         Collider[] hit = Physics.OverlapSphere(transform.position, aggroRange);
         foreach(Collider col in hit)
@@ -108,18 +189,24 @@ public class EnemyAI : MonoBehaviour
             }
         }
 
-        if (inRange && !playerIsHidden)
+        //Check if the player is in sight range
+        if (inRange)
         {
-            playerInSight = true;
-            isAggrod = true;
-            ai.destination = player.position;
-        }
-        else if(playerIsHidden)
-        {
-            playerInSight = false;
-        }  
+            //Check if player is hidden
+            if (!playerIsHidden)
+            {
+                print("Spotted the player");
+                playerInSight = true;
+                isAggrod = true;
+            }
+            else
+            {
+                playerInSight = false;
+            }
+        }        
     }
 
+    //If enemy is aggrod but player is not in sight
     void BeginDeaggro()
     {
         if(!idle && isAggrod && !playerInSight)
@@ -132,6 +219,7 @@ public class EnemyAI : MonoBehaviour
         }               
     }
 
+    //Wait a set amount of time before returning to patrol. Reaggro if player is in sight.
     IEnumerator LoseAggro()
     {
         int secondsElapsed = 0;
