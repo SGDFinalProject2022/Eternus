8d73b6fcec5c038f03a61eea4ca58eb9c2a60082
epicjodog <epicjodog@gmail.com>
2022-05-17 21:42:25 -0400

    Basic Player Movement + framework stuff
    this has a basic player inside ./Prefabs
    i also added a folder inside scripts dedicated to player movement
    i also added a test map and placeholder materials for testing

---
+++ Eternus/Assets/Scripts/PlayerInteractions/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates the player camera according to mouse input
+/// </summary>
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 500f;
+    [SerializeField] Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //something makes the mouse jump..but what?
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
