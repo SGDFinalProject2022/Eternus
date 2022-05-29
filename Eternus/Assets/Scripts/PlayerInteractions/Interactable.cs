6ad4c5e76b070548fc1e3706898f5d5655334799
epicjodog <epicjodog@gmail.com>
2022-05-28 22:38:50 -0400

    Door SFX
    - imported door sfx, new "interactables" folder in SFX
    
    - plays the jiggling sfx when interacting with a locked door
    
    - Added new functions to my AudioMan: PlayForceEntirely("(sound)"):  forces a sound to play for it's entirety before being able to play again
    PlayForceEntirely("(sound)", (audio clip)): like the last one but plays the new audio clip instead
    ...this could have been just another line of code in Interactable but it didn't sit with me right... orz

--- Eternus/Assets/Scripts/PlayerInteractions/Interactable.cs
+++ Eternus/Assets/Scripts/PlayerInteractions/Interactable.cs
@@ -10,7 +10,10 @@ public class Interactable : MonoBehaviour
     public UnityEvent onInteract;
     public int ID;
     public string interactText;
+
+    [Header("Door Stuff")]
     public bool isUnlocked = true;
+    [SerializeField] AudioClip[] jiggleSFX;
 
     // Start is called before the first frame update
     void Start()
@@ -31,15 +34,18 @@ public class Interactable : MonoBehaviour
 
     public void CheckHasKey()
     {
+        AudioManager audioMan = GetComponent<AudioManager>();
         if(isUnlocked)
         {
+            audioMan.PlayForceEntirely("Open"); //when we have an animation for the door, this will play
             FindObjectOfType<UI>().ShowObjective("Find a key: Complete");
             print(this.gameObject.name + " will open (probably using some door open animation)");
             gameObject.layer = 0;
-            gameObject.SetActive(false); //remove when there is an animation
+            gameObject.SetActive(false); //remove when there is an animation!!
         }
         else
         {
+            audioMan.PlayForceEntirely("Locked", jiggleSFX[Random.Range(0, jiggleSFX.Length - 1)]);
             FindObjectOfType<UI>().ShowObjective("Objective: Find a key");
             interactText = "locked. find a key";
         }
