0d51851849331a85b8144d51d9a1cabba60d20a0
vendorko <73616316+vendorko@users.noreply.github.com>
2022-06-05 15:14:27 -0400

    Made credits loop back to main menu

--- Eternus/Assets/Scripts/ScrollingText.cs
+++ Eternus/Assets/Scripts/ScrollingText.cs
@@ -6,9 +6,22 @@ public class ScrollingText : MonoBehaviour
 {
     [SerializeField] GameObject textBox;
     [SerializeField] float scrollSpeed = 3f;
+    [SerializeField] float creditsTimer = 20f;
+
+    void Start()
+    {
+        textBox.transform.position = new Vector2(Screen.width/2, -75);
+        StartCoroutine("Countdown");
+    }
 
     void Update()
     {
         textBox.transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed, Space.World);
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(creditsTimer);
+        GlobalData.instance.LoadScene("MainMenu");
     }
 }
