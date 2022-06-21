1fe9de95571eba1652358043091f865f7541bba1
vendorko <73616316+vendorko@users.noreply.github.com>
2022-06-21 18:56:30 -0400

    Pushed note for floor 2

---
+++ Eternus/Assets/Scripts/NoteInteraction.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInteraction : MonoBehaviour
+{
+    [SerializeField] List<GameObject> panels = new List<GameObject>();
+    [SerializeField] UI ui;
+
+    bool panelIsOpen;
+    PlayerMovement mov;
+
+    void Awake()
+    {
+        foreach (GameObject obj in panels)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            ClosePanel();
+        }
+    }
+
+    public void ClosePanel()
+    {
+        if (panelIsOpen)
+        {
+            ui.headBobController.enableHeadbob = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            panelIsOpen = false;
+            ui.move.enabled = true;
+            ui.look.enabled = true;
+            foreach (GameObject obj in panels)
+            {
+                obj.SetActive(false);
+            }
+            StartCoroutine("AllowPause");
+        }
+    }
+
+    IEnumerator AllowPause()
+    {
+        yield return new WaitForSeconds(.025f);
+        ui.panelIsOpen = false;
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        if (!panelIsOpen)
+        {
+            ui.headBobController.enableHeadbob = false;
+            Cursor.lockState = CursorLockMode.None;
+            ui.panelIsOpen = true;
+            ui.move.enabled = false;
+            ui.look.enabled = false;
+            panelIsOpen = true;
+            panel.SetActive(true);
+        }
+    }
+}
